35e1adffaec88feb9f31620b476467625a1116b3
Ling Zhang <ling.zhang@nytimes.com>
2019-04-01 12:06:06 -0400

    add validateAndPreprocessInputParams function to allow customized ttl input when create new tokens

--- plugin/path_generate.go
+++ plugin/path_generate.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"strconv"
 	"strings"
 	"time"
 
@@ -32,15 +33,24 @@ func (b *backend) pathGenerate(ctx context.Context, req *logical.Request, data *
 
 	config, err := b.config(ctx, req.Storage)
 
-	scope := data.Get("scope").(string)
-	serviceIDs := strings.Split(data.Get("service_id").(string), ",")
+	// Validate the data passed in the field and preprocess them so they can be used directly for generating tokens
+	validatedData, err := validateAndPreprocessInputParams(data)
+
+	if err != nil {
+		return &logical.Response{
+			Data: map[string]interface{}{
+				"error": err.Error(),
+			},
+		}, nil
+	}
 
 	formData := map[string][]string{
 		"username":   []string{config.Username},
 		"password":   []string{config.Password},
-		"scope":      []string{scope},
+		"scope":      validatedData["scope"],
+		"services[]": validatedData["serviceIDs"],
 		"name":       []string{"vault-fastly-secret-engine"},
-		"services[]": serviceIDs,
+		"expires_at": validatedData["expires_at"],
 	}
 
 	totp, err := generateTOTPCode(config.SharedSecret)
@@ -70,8 +80,6 @@ func (b *backend) pathGenerate(ctx context.Context, req *logical.Request, data *
 
 // CreateFastlyToken uses Fastly API to create an API token
 func CreateFastlyToken(otp string, formData map[string][]string) (*FastlyToken, error) {
-	// Make sure the token expires
-	formData = ensureExpirationInParams(formData)
 
 	// Prepare request
 	form := url.Values(formData)
@@ -119,12 +127,39 @@ func CreateFastlyToken(otp string, formData map[string][]string) (*FastlyToken, 
 	return token, nil
 }
 
+func validateAndPreprocessInputParams(data *framework.FieldData) (map[string][]string, error) {
+
+	scope := data.Get("scope").(string)
+	serviceIDs := strings.Split(data.Get("service_id").(string), ",")
+
+	originTTLString := data.Get("ttl").(string)
+
+	if len(originTTLString) == 0 {
+		originTTLString = "300"
+	}
+
+	ttl, err := strconv.Atoi(originTTLString)
+	if err != nil {
+		return nil, fmt.Errorf("Invalid ttl value %s", originTTLString)
+	}
+
+	expiresAt := time.Now().Add(time.Duration(ttl) * time.Minute).UTC().Format("2006-01-02T15:04:05+00:00")
+
+	validatedData := map[string][]string{
+		"scope":      []string{scope},
+		"services[]": serviceIDs,
+		"expires_at": []string{expiresAt},
+	}
+
+	return validatedData, err
+}
+
 func ensureExpirationInParams(formData map[string][]string) map[string][]string {
 	// This adds expiration to create token POST form if one doesn't exist
 	// Default TTL 5 minutes for now
 	defaultExpiration := time.Now().Add(5 * time.Minute).UTC().Format("2006-01-02T15:04:05+00:00")
 
-	field := formData["expires_at"]
+	field := formData["ttl"]
 	if field == nil || len(field) == 0 {
 		formData["expires_at"] = []string{defaultExpiration}
 	}
